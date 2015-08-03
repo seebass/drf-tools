ee55f4304274984bb4ab6a6d18892045f6fcb142
Sebastian Bredehöft <sebastian.bredehoeft@comsulting.de>
2015-08-03 14:52:01 +0200

    CHANGED: map integrity error to 400

--- drf_tools/exceptions.py
+++ drf_tools/exceptions.py
@@ -1,11 +1,11 @@
 import logging
 
 from django.core.exceptions import ValidationError, ObjectDoesNotExist, PermissionDenied
+from django.db import IntegrityError
 from django.http import Http404
 from rest_framework.exceptions import APIException
 from rest_framework import status
 from rest_framework.response import Response
-
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +19,7 @@ def exception_handler(exc):
             headers['X-Throttle-Wait-Seconds'] = '%d' % exc.wait
             headers['Retry-After'] = '%d' % exc.wait
         status_code = exc.status_code
-    elif isinstance(exc, (ValueError, ValidationError)):
+    elif isinstance(exc, (ValueError, ValidationError, IntegrityError)):
         status_code = status.HTTP_400_BAD_REQUEST
     elif isinstance(exc, PermissionDenied):
         status_code = status.HTTP_403_FORBIDDEN
