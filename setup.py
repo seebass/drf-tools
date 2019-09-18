5c8eceb6f26ef48757045898f661141b9fc71da7
Tim Mundt <dev@tim-erwin.de>
2019-09-18 13:23:47 +0200

    increased version to 0.9.37

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='drf-tools',
-    version="0.9.36",
+    version="0.9.37",
     url='https://github.com/seebass/drf-tools',
     license='MIT',
     description='Multiple extensions and test utilities for Django REST Framework 3',
